9309493484a1a0c106b09eef690e8ce57f92aa6f
Gustavo Gantois <gugagantois@gmail.com>
2024-12-13 00:03:52 -0300

    style:Extend variants and adding AOS Animation in navbar desktop.

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -6,17 +6,59 @@ export function Navbar() {
     return (
         <nav className="max-w-[1550px] mx-auto p-8 lg:p-6 font-primary">
             <div className="flex justify-around items-center gap-40">
-            <Image src="/Logo.svg" width={70} height={200} quality={100} alt="" />
-            <ul className="flex justify-center gap-6 text-sm">
-                <a href="#Home">Home</a>
-                <a href="#About">Sobre mim</a>
-                <a href="#Services">Meus Serviços</a>
-                <a href="#Testimonials">Depoimentos</a>
-                <a href="#FAQ">FAQ</a>
-                <a href="#Location">Localização</a>
-                <a href="#Contact">Contato</a>
+            <Image src="/Logo.svg" width={70} height={200} quality={100} alt="" data-aos="fade-down" />
+            <ul className="flex justify-center gap-6 text-sm" data-aos="fade-down" data-aos-delay="100">
+            <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#Home"
+  >
+    Home
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#About"
+  >
+    Sobre mim
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#Services"
+  >
+    Meus Serviços
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#Testimonials"
+  >
+    Depoimentos
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#FAQ"
+  >
+    FAQ
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#Location"
+  >
+    Localização
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
+  <Link
+    className="relative text-[#262626] hover:text-[#0094c7] transition-all duration-500 group"
+    href="#Contact"
+  >
+    Contato
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#0094c7] transition-all duration-500 group-hover:w-full"></span>
+  </Link>
             </ul>
-            <Link href="" target="_blank">
+            <Link href="" target="_blank" data-aos="fade-down" data-aos-delay="200">
             <Button className="p-5" variant="secondary">Agende a sua consulta</Button>
             </Link>
             </div>
