3cec47841126d4cf1a6facf5a3df7d4da72081fc
Gustavo Gantois <gugagantois@gmail.com>
2024-12-14 10:31:17 -0300

    style:Choose new image for session about.

--- src/app/s-contact.tsx
+++ src/app/s-contact.tsx
@@ -1,7 +1,9 @@
 export function HomeContact() {
     return (
-        <section>
-            
+        <section className="max-w-[1550px] mx-auto p-6 lg:p-0">
+            <div className="flex flex-col gap-6">
+
+            </div>
         </section>
     )
 }
